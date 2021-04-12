a4245df9421909a584824d44c802989f322ffdc4
Leandro Maciel <leandrojmp@gmail.com>
2021-04-12 00:02:56 -0300

    feat: read file
    first test reading file

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
 )
 
 type Mappings struct {
@@ -10,16 +12,34 @@ type Mappings struct {
 }
 
 type Properties struct {
-	Properties string `json:"properties"`
+	Properties interface{} `json:"properties"`
+}
+
+type Type struct {
+	Type string `json:"type"`
+}
+
+func ReadFile(mappingFile string) []string {
+	content, err := os.ReadFile(mappingFile)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	lines := strings.Split(string(content), "\n")
+	return lines
 }
 
 func main() {
 	jsonData := Mappings{
 		Mappings: Properties{
-			Properties: "teste",
+			Properties: Type{
+				Type: "ip",
+			},
 		},
 	}
 	mappingFile, _ := json.MarshalIndent(jsonData, "", "    ")
 	fmt.Printf("%+v\n", string(mappingFile))
+	for _, line := range ReadFile("mapping.txt") {
+		fmt.Print(line + string('\n'))
+	}
 	//_ = ioutil.WriteFile("mappings.json", mappingFile, 0644)
 }
