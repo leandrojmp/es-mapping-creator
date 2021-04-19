5cd9c3e1949e684ca62d74f9d95bbfa676c966e4
Leandro Maciel <leandrojmp@gmail.com>
2021-04-18 22:34:32 -0300

    feat: mapping for nested fields
    create mapping for one level nested fields in the format first.second: type

--- main.go
+++ main.go
@@ -15,6 +15,10 @@ type Properties struct {
 	Properties interface{} `json:"properties"`
 }
 
+type Type struct {
+	Type string `json:"type"`
+}
+
 func CreateMapping(mappingFile string) interface{} {
 	// read file and create list with the content
 	content, err := os.ReadFile(mappingFile)
@@ -24,10 +28,33 @@ func CreateMapping(mappingFile string) interface{} {
 	lines := strings.Split(string(content), "\n")
 	// for each line add item as a key in a dict with the type
 	mappingFields := make(map[string]interface{})
+	nestedFields := make(map[string]map[string]interface{})
+	topFields := make(map[string]string)
+	var nestedList []string
+	// check for nested, works only for two level in the format first.second: type
 	for _, line := range lines {
-		typeField := make(map[string]interface{})
-		typeField["type"] = strings.Split(line, ": ")[1]
-		mappingFields[strings.Split(line, ": ")[0]] = typeField
+		fieldName := strings.Split(line, ": ")[0]
+		fieldType := strings.Split(line, ": ")[1]
+		if strings.Contains(fieldName, ".") {
+			nestedList = append(nestedList, strings.Replace(line, ": ", "-", 1))
+		} else {
+			mappingFields[fieldName] = Type{fieldType}
+		}
+	}
+	// loop nested fields list
+	for _, field := range nestedList {
+		fieldTop := strings.Split(strings.Split(field, "-")[0], ".")[0]
+		topFields[fieldTop] = fieldTop
+		if nestedFields[fieldTop] == nil {
+			nestedFields[fieldTop] = make(map[string]interface{})
+		}
+		fieldName := strings.Split(strings.Split(field, "-")[0], ".")[1]
+		fieldType := strings.Split(field, "-")[1]
+		nestedFields[fieldTop][fieldName] = Type{fieldType}
+	}
+	// create mapping for top level nested fields
+	for _, top := range topFields {
+		mappingFields[top] = Properties{nestedFields[top]}
 	}
 	return mappingFields
 }
@@ -40,4 +67,5 @@ func main() {
 	}
 	mappingFile, _ := json.MarshalIndent(jsonData, "", "    ")
 	_ = os.WriteFile("es-mappings.json", mappingFile, 0644)
+	// fmt.Printf("%+v\n", string(mappingFile))
 }
